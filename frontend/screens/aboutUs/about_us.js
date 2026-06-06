26d719fdf03dae4ddd7072d6d82499e01381ef52
ilu <naila.riani@gmail.com>
2026-06-06 16:37:34 +0700

    Add files via upload

---
+++ frontend/screens/aboutUs/about_us.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("STOKKO About Us page loaded.");
+});
---
+++ frontend/screens/landing/landing.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.testi-swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            }
+        }
+    });
+});
